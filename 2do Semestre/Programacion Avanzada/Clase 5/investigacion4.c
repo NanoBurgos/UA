17a6118e7740dbf8d6a67276f3c6d997e29f817f
n <nanovonlepel@gmail.com>
2026-04-11 19:04:44 -0300

    Investigacion, ñ(enhe) en C, metodo 4

--- 2do Semestre/Programacion Avanzada/Clase 5/investigacion4.c
+++ 2do Semestre/Programacion Avanzada/Clase 5/investigacion4.c
@@ -15,4 +15,14 @@ int main(){
     return 0;
 }
 
-/*Esto no funcionara en todos los sistemas operativos*/
+/*OBS: Esto no funcionara en todos los sistemas operativos
+
+1. #include <stdio.h>: Incluye la biblioteca estándar de entrada y salida.
+
+2. #include <locale.h>: Incluye la biblioteca para configurar la localización, lo que permite el uso de caracteres especiales.
+
+3. setlocale(LC_ALL, "");: Configura la localización para que el programa pueda manejar caracteres especiales según la configuración regional del sistema.
+
+4. printf: Función para imprimir en pantalla. Aquí se utilizan caracteres especiales como "ñ" y vocales acentuadas.
+
+*/
